c3fdf156ea2885faa19ddbc677c9ea5ef01e4053
AmrAmrA <amrelfattah.dev@gmail.com>
2023-11-20 16:14:53 +0100

    optimize LodgementLocation logic and add some types

--- src/Components/LodgementLocation/LodgementLocation.tsx
+++ src/Components/LodgementLocation/LodgementLocation.tsx
@@ -1,20 +1,28 @@
-import React from "react";
 import { useParams } from "react-router-dom";
 import LodgmentData from "../../logements.json";
 import "./__LodgementLocation.scss";
 
+interface RouteParams {
+  id: string;
+}
+
+interface Card {
+  title : string; 
+  location : string; 
+  id : string; 
+}
 export default function LodgementLocation() {
-  const params = useParams();
+  const params = useParams() as any as RouteParams;
+  const currentCard = LodgmentData.find((card: Card) => card.id === params.id);
+
+  if (!currentCard) {
+    return <div>Logement introuvable.</div>;
+  }
+
   return (
-    <>
-      {LodgmentData.filter((card) => card.id === params.id).map(
-        (card, index) => (
-          <div key = {index}>
-            <h1>{card.title}</h1>
-            <p>{card.location}</p>
-          </div>
-        )
-      )}
-    </>
+    <div>
+      <h1>{currentCard.title}</h1>
+      <p>{currentCard.location}</p>
+    </div>
   );
 }
